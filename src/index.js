67d6aa245de0d2286a6600e2efb67cf9b7b903ab
caoshengxiang <1104729815@qq.com>
2017-08-02 11:36:37 +0800

    mint-ui

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import App from './app.vue'
 import store from './store'
 import './styles/basic.scss'
 import i18n from './i18n'
+import Mint from 'mint-ui'
+import 'mint-ui/lib/style.css'
 
-
+Vue.use(Mint);
 new Vue({
     el: '#app',
     store,

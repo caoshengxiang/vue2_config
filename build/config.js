252c274805ef6c7674dd13c65b1e53b3e5e62796
caoshengxiang <1104729815@qq.com>
2017-08-04 14:16:47 +0800

    安装video.js

--- build/config.js
+++ build/config.js
@@ -6,7 +6,7 @@ module.exports = {
     dev: {
         // host: '192.168.1.127',  // 主机或IP
         host: 'localhost',  // 主机或IP
-        port: 8800, // 设置端口号
+        port: 8101, // 设置端口号
         assetsPublicPath: '',
         contentBase: path.join(__dirname, '/../dist'),
         proxyTable: { // 设置代理服务器

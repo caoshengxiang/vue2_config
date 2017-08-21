6ae886f304f8c21d8e38f2e74d64b4e18884b7d9
caoshengxiang <1104729815@qq.com>
2017-08-21 17:07:59 +0800

    修改awesome目录

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './styles/basic.scss'
 import i18n from './i18n'
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
-import './awesome'
+import './components/awesome'
 
 Vue.use(Mint);
 new Vue({

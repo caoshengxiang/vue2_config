22680ecf6549683d5d90856e7b2d6401447d1b6a
caoshengxiang <1104729815@qq.com>
2017-08-30 20:33:39 +0800

    debug

--- src/pages/home/home_pc.vue
+++ src/pages/home/home_pc.vue
@@ -23,7 +23,10 @@
             <img class="living" src="../../assets/home/living.png" alt="">
         </div>
         <div class="part-2">
-            下 载 即 代 表 您 同 意&nbsp;&nbsp; <router-link to="/pc/user/pro" target="_blank">铸 魂 用 户 协 议</router-link>
+            <div class="text">
+                下 载 即 代 表 您 同 意&nbsp;&nbsp; <router-link to="/pc/user/pro" target="_blank">铸 魂 用 户 协 议</router-link>
+            </div>
+
         </div>
     </div>
 </template>
@@ -130,6 +133,9 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        .text {
+            text-align: center;
+        }
         a {
             color: red;
             text-decoration: underline;

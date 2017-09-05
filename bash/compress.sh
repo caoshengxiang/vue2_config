87291ba231de30c5a60f040cc93e9841c19236ca
caoshengxiang <1104729815@qq.com>
2017-09-05 16:34:35 +0800

    修改服务器上传地址

--- bash/compress.sh
+++ bash/compress.sh
@@ -2,7 +2,7 @@
 # && 如果前面的命令正确执行，那么后面的命令也将执行。否则后面的命令不执行。
 mv dist zhlive-weixin && tar zcvf zhlive-weixin.tar.gz zhlive-weixin && mv zhlive-weixin dist && mv zhlive-weixin.tar.gz dist/
 scp dist/zhlive-weixin.tar.gz root@47.95.6.138:/opt
-ssh root@47.95.6.138 "rm -rf /opt/tomcat/webapps/weixin && tar -zxvf /opt/zhlive-weixin.tar.gz"
-ssh root@47.95.6.138 "mv zhlive-weixin /opt/tomcat/webapps/weixin"
+ssh root@47.95.6.138 "rm -rf /opt/www/weixin && tar -zxvf /opt/zhlive-weixin.tar.gz"
+ssh root@47.95.6.138 "mv zhlive-weixin /opt/www/weixin"
 
 # Chengxun@1806

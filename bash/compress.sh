b7059415a10cdb9a80482f910f61290ab63a51ff
caoshengxiang <1104729815@qq.com>
2017-09-05 16:33:28 +0800

    修改服务器上传地址

--- bash/compress.sh
+++ bash/compress.sh
@@ -2,7 +2,7 @@
 # && 如果前面的命令正确执行，那么后面的命令也将执行。否则后面的命令不执行。
 mv dist zhlive-home && tar zcvf zhlive-home.tar.gz zhlive-home && mv zhlive-home dist && mv zhlive-home.tar.gz dist/
 scp dist/zhlive-home.tar.gz root@47.95.6.138:/opt
-ssh root@47.95.6.138 "rm -rf /opt/tomcat/webapps/home && tar -zxvf /opt/zhlive-home.tar.gz"
-ssh root@47.95.6.138 "mv zhlive-home /opt/tomcat/webapps/home"
+ssh root@47.95.6.138 "rm -rf /opt/www/home && tar -zxvf /opt/zhlive-home.tar.gz"
+ssh root@47.95.6.138 "mv zhlive-home /opt/www/home"
 
 # Chengxun@1806
